d91ba1d99be394f9c5ba40effbf4887f92a1e533
matteo <matteo.casiraghi@issgreppi.it>
2023-02-27 10:59:40 +0100

    1.2.13.6.7.3 Utilizzo di un ViewModel per il passaggio di dati strutturati complessi dal Controller alla View - best practice

---
+++ BulkyBook.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; } = null!;
+        public IEnumerable<SelectListItem> CategoryList { get; set; } = null!;
+        public IEnumerable<SelectListItem> CoverTypeList { get; set; } = null!;
+
+    }
+}
