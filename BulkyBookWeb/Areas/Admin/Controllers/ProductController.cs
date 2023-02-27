7948d533e8dd40b35c4613ac93b9ff853fbac920
matteo <matteo.casiraghi@issgreppi.it>
2023-02-27 10:49:26 +0100

    Aggiunte dropdownList sulla View Upsert

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBookWeb.Controllers
 {
@@ -25,57 +26,59 @@ namespace BulkyBookWeb.Controllers
         }
 
         //GET
-        public IActionResult Create()
+
+
+
+
+
+        public IActionResult Upsert(int? id)
         {
-            return View();
-        }
+            Product product = new();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+            IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(CoverType obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.CoverType.Add(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Category created successfully";
-                return RedirectToAction(nameof(Index));
-            }
-            return View(obj);
-
-        }
-
-        //GET
-        public IActionResult Edit(int? id)
-        {
             if (id == null || id == 0)
             {
-                return NotFound();
+                //esempio di uso di ViewBag
+                ViewBag.CategoryList = CategoryList;
+                //esempio di uso di ViewData
+                ViewData["CoverTypeList"] = CoverTypeList;
+
+
+                //create product
+                return View(product);
             }
-            //var categoryFromDb = _db.Categories.Find(id);
-            var coverTypeFromDbFirst = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
-            if (coverTypeFromDbFirst == null)
+            else
             {
-                return NotFound();
+                //update product
             }
-            return View(coverTypeFromDbFirst);
+            return View(product);
         }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(CoverType obj)
+
+        public IActionResult Upsert(Product obj)
         {
-            
             if (ModelState.IsValid)
             {
-                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Product.Update(obj);
                 _unitOfWork.Save();
-                TempData["success"] = "Category updated successfully";
+                TempData["success"] = "Product updated successfully";
                 return RedirectToAction(nameof(Index));
             }
             return View(obj);
-
         }
 
 
