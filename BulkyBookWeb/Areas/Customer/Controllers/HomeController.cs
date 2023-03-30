257a58b9b83566155c32816bf181b3e93c5ed916
Matteo Casiraghi <MATTEO.CASIRAGHI@issgreppi.it>
2023-03-30 11:22:43 +0200

    1.2.13.11.2 Aggiunta di nuovi prodotti e aggiornamento di prodotti già presenti nella Shopping Cart

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkyBookWeb.Controllers;
 [Area("Customer")]
@@ -9,20 +11,11 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IUnitOfWork _unitOfWork;
-
-    //private readonly IUnitOfWork _unitOfWork;
     public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
         _unitOfWork = unitOfWork;
     }
-
-
-    //public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-    //{
-    //    _logger = logger;
-    //    _unitOfWork = unitOfWork;
-    //}
 
     public IActionResult Index()
     {
@@ -30,6 +23,7 @@ public class HomeController : Controller
         //return View(productList);
         return View();
     }
+
     public IActionResult Details(int productId)
     {
         var selectedProductInDb = _unitOfWork.Product.GetFirstOrDefault(product => product.Id == productId, "Category,CoverType");
@@ -38,16 +32,52 @@ public class HomeController : Controller
             ShoppingCart cartObj = new()
             {
                 Count = 1,
+                ProductId = productId,
                 Product = selectedProductInDb
 
             };
             return View(cartObj);
         }
-
         return RedirectToAction(nameof(Index));
-
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [Authorize]
+    public IActionResult Details(ShoppingCart shoppingCart)
+    {
+        var userIdentity = User.Identity;
+        if (userIdentity != null)
+        {
+            var claimsIdentity = (ClaimsIdentity)userIdentity;
+            var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                //aggiorno il riferimento all'utente
+                shoppingCart.ApplicationUserId = claim.Value;
+                //controllo che l'Id passato nel form corrisponda effettivamente a un prodotto nel database
+                Product? selectedProductInDb = _unitOfWork.Product.GetFirstOrDefault(product => product.Id == shoppingCart.ProductId, "Category,CoverType");
+                if (selectedProductInDb != null)
+                {
+                    //verifico se c'è già un prodotto con lo stesso id nella shopping cart (nel database)
+                    ShoppingCart? cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
+
+                    if (cartFromDb == null)//il prodotto non è presente nella shopping cart --> Add
+                    {
+                        //salvo shoppingCart: ha valori per ProductId, ApplicationUserId e Count. L'Id verrà definito dal database
+                        _unitOfWork.ShoppingCart.Add(shoppingCart);
+                    }
+                    else //il prodotto è già presente nella shopping cart --> Update tramite aggiornamento dell'oggetto tracciato da EF Core e salvataggio
+                    {
+                        _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
+                    }
+                    _unitOfWork.Save();
+                    RedirectToAction(nameof(Index));
+                }
+            }
+        }
+        return RedirectToAction(nameof(Index));
+    }
 
     public IActionResult Privacy()
     {
