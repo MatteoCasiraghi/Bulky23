e0666cabf66b21be54e4ac4422183b2b950661b3
Matteo Casiraghi <MATTEO.CASIRAGHI@issgreppi.it>
2023-04-04 10:31:10 +0200

    1.2.13.11.5 Aggiornamento della quantità nella ShoppingCart

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -6,46 +6,86 @@ using System.Security.Claims;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    [Authorize]
-    public class CartController : Controller
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public ShoppingCartVM ShoppingCartVM { get; set; }
+	[Area("Customer")]
+	[Authorize]
+	public class CartController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+		public ShoppingCartVM ShoppingCartVM { get; set; }
 
-        public CartController(IUnitOfWork unitOfWork)
-        {
-            _unitOfWork = unitOfWork;
-            ShoppingCartVM = new ShoppingCartVM();
-        }
-        public IActionResult Index()
-        {
-            var userIdentity = User.Identity;
-            if (userIdentity != null)
-            {
-                var claimsIdentity = (ClaimsIdentity)userIdentity;
-                var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
-                if (claim != null)
-                {
-                    ShoppingCartVM = new ShoppingCartVM()
-                    {
-                        ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
-                    };
-                    foreach (var cart in ShoppingCartVM.ListCart)
-                    {
-                        cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
-                        ShoppingCartVM.CartTotal += cart.Price * cart.Count;
-                    }
-                }
-            }
-            return View(ShoppingCartVM);
-        }
-        private static double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100) => quantity switch
-        {
-            <= 50 => price,
-            <= 100 => price50,
-            _ => price100,
-        };
+		public CartController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+			ShoppingCartVM = new ShoppingCartVM();
+		}
+		public IActionResult Index()
+		{
+			var userIdentity = User.Identity;
+			if (userIdentity != null)
+			{
+				var claimsIdentity = (ClaimsIdentity)userIdentity;
+				var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier);
+				if (claim != null)
+				{
+					ShoppingCartVM = new ShoppingCartVM()
+					{
+						ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
+					};
+					foreach (var cart in ShoppingCartVM.ListCart)
+					{
+						cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+						ShoppingCartVM.CartTotal += cart.Price * cart.Count;
+					}
+				}
+			}
+			return View(ShoppingCartVM);
+		}
 
-    }
+		public IActionResult Plus(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+			if (cart != null)
+			{
+				_unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+				_unitOfWork.Save();
+			}
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Minus(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+			if (cart != null)
+			{
+				if (cart.Count <= 1)
+				{
+					_unitOfWork.ShoppingCart.Remove(cart);
+				}
+				else
+				{
+					_unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+				}
+				_unitOfWork.Save();
+			}
+			return RedirectToAction(nameof(Index));
+		}
+
+		public IActionResult Remove(int cartId)
+		{
+			var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+			if (cart != null)
+			{
+				_unitOfWork.ShoppingCart.Remove(cart);
+				_unitOfWork.Save();
+			}
+			return RedirectToAction(nameof(Index));
+		}
+		private static double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100) => quantity switch
+		{
+			<= 50 => price,
+			<= 100 => price50,
+			_ => price100,
+		};
+
+	}
 }
